4c44f180a4d7320898810a4a93cebdc4f56c06e7
Splitwirez <Splitwirez@gmail.com>
2020-04-17 19:15:18 -0400

    Added `FlyoutButton`

---
+++ Mechanism.AvaloniaUI/Controls/FlyoutButton.cs
@@ -0,0 +1,66 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Avalonia.Input;
+using Avalonia.Styling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mechanism.AvaloniaUI.Controls
+{
+    public class FlyoutButton : ToggleButton, IStyleable
+    {
+        /*public static readonly StyledProperty<bool> IsFlyoutOpenProperty =
+            AvaloniaProperty.Register<OverflowFlyoutItemsControl, bool>(nameof(IsFlyoutOpen), defaultValue: false);
+
+        public bool IsFlyoutOpen
+        {
+            get => GetValue(IsFlyoutOpenProperty);
+            set => SetValue(IsFlyoutOpenProperty, value);
+        }*/
+
+        Type IStyleable.StyleKey => typeof(FlyoutButton);
+
+        public static readonly StyledProperty<object> FlyoutContentProperty =
+            AvaloniaProperty.Register<FlyoutButton, object>(nameof(FlyoutContent));
+
+        public object FlyoutContent
+        {
+            get => GetValue(FlyoutContentProperty);
+            set => SetValue(FlyoutContentProperty, value);
+        }
+
+        public static readonly StyledProperty<IDataTemplate> FlyoutContentTemplateProperty =
+            AvaloniaProperty.Register<FlyoutButton, IDataTemplate>(nameof(FlyoutContentTemplate));
+
+        public IDataTemplate FlyoutContentTemplate
+        {
+            get => GetValue(FlyoutContentTemplateProperty);
+            set => SetValue(FlyoutContentTemplateProperty, value);
+        }
+
+        public static readonly StyledProperty<IInputElement> FocusOnOpenElementProperty =
+            AvaloniaProperty.Register<FlyoutButton, IInputElement>(nameof(FocusOnOpenElement));
+
+        public IInputElement FocusOnOpenElement
+        {
+            get => GetValue(FocusOnOpenElementProperty);
+            set => SetValue(FocusOnOpenElementProperty, value);
+        }
+
+        static FlyoutButton()
+        {
+            IsCheckedProperty.Changed.AddClassHandler<FlyoutButton>(new Action<FlyoutButton, AvaloniaPropertyChangedEventArgs>((sender, e) =>
+            {
+                if (e.NewValue is bool isChecked)
+                {
+                    if (isChecked)
+                        sender.FocusOnOpenElement?.Focus();
+                    else
+                        sender.Focus();
+                }
+            }));
+        }
+    }
+}
