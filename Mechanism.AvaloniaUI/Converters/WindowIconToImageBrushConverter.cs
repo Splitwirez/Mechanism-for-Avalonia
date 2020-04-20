3221587a76cd819c49617b6a1125231798a41666
Splitwirez <Splitwirez@gmail.com>
2020-04-20 08:23:25 -0400

    Fix `WindowIconToImageBrushConverter`

--- Mechanism.AvaloniaUI/Converters/WindowIconToImageBrushConverter.cs
+++ Mechanism.AvaloniaUI/Converters/WindowIconToImageBrushConverter.cs
@@ -18,33 +18,39 @@ namespace Mechanism.AvaloniaUI.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var wIcon = value as WindowIcon;
-            MemoryStream stream = new MemoryStream();
-            //if (icon.PlatformImpl is IconImpl impl)
-            wIcon.Save(stream);
-            try
+            if (value != null)
             {
-                return new ImageBrush(new Bitmap(stream));
-            }
-            catch (ArgumentNullException ex)
-            {
+                var wIcon = value as WindowIcon;
+                MemoryStream stream = new MemoryStream();
+                //if (icon.PlatformImpl is IconImpl impl)
+                wIcon.Save(stream);
+                stream.Position = 0;
                 try
                 {
-
-                    Icon icon = new Icon(stream);
-                    System.Drawing.Bitmap bmp = icon.ToBitmap();
-                    bmp.Save(stream, ImageFormat.Png);
                     return new ImageBrush(new Bitmap(stream));
                 }
-                catch (ArgumentException e)
+                catch (ArgumentNullException ex)
                 {
-                    Icon icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetEntryAssembly().Location);
-                    System.Drawing.Bitmap bmp = icon.ToBitmap();
-                    Stream stream3 = new MemoryStream();
-                    bmp.Save(stream3, ImageFormat.Png);
-                    return new ImageBrush(new Bitmap(stream3));
+                    try
+                    {
+
+                        Icon icon = new Icon(stream);
+                        System.Drawing.Bitmap bmp = icon.ToBitmap();
+                        bmp.Save(stream, ImageFormat.Png);
+                        return new ImageBrush(new Bitmap(stream));
+                    }
+                    catch (ArgumentException e)
+                    {
+                        Icon icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetEntryAssembly().Location);
+                        System.Drawing.Bitmap bmp = icon.ToBitmap();
+                        Stream stream3 = new MemoryStream();
+                        bmp.Save(stream3, ImageFormat.Png);
+                        return new ImageBrush(new Bitmap(stream3));
+                    }
                 }
             }
+            else
+                return new ImageBrush();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
