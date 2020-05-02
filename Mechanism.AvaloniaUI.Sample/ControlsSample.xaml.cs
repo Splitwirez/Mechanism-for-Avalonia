1219b42935279b49c42905e094f4b39731cba415
Splitwirez <Splitwirez@gmail.com>
2020-05-02 06:44:55 -0400

    Fluent Theme: *exists*

---
+++ Mechanism.AvaloniaUI.Sample/ControlsSample.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Mechanism.AvaloniaUI.Sample
+{
+    public class ControlsSample : UserControl
+    {
+        public ControlsSample()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ Mechanism.AvaloniaUI/Drawing/DrawingExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Mechanism.AvaloniaUI.Drawing
+{
+    public static class DrawingExtensions
+    {
+        public static System.Drawing.Image ToDrawingImage(this Avalonia.Media.Imaging.Bitmap bmp)
+        {
+            System.Drawing.Image ret;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                bmp.Save(stream);
+                ret = System.Drawing.Image.FromStream(stream);
+            }
+            return ret;
+        }
+
+        public static System.Drawing.Bitmap ToDrawingBitmap(this Avalonia.Media.Imaging.Bitmap bmp) => (System.Drawing.Bitmap)bmp.ToDrawingImage();
+
+        public static Avalonia.Media.Imaging.Bitmap ToMediaBitmap(this System.Drawing.Bitmap bmp)
+        {
+            Avalonia.Media.Imaging.Bitmap ret;
+            /*var data = bmp.LockBits(new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            using (MemoryStream stream = new MemoryStream())
+            {
+                bmp.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                ret = new Avalonia.Media.Imaging.Bitmap(stream);
+            }
+            return ret;*/
+            using (var stream = new MemoryStream())
+            {
+                bmp.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+
+                stream.Position = 0;
+
+                //var factory = Avalonia.AvaloniaLocator.Current.GetService(typeof(Avalonia.Platform.IPlatformRenderInterface));
+
+                //ret = factory.LoadBitmap(stream);
+                ret = new Avalonia.Media.Imaging.Bitmap(stream);
+            }
+            return ret;
+        }
+
+        public static System.Drawing.PointF ToDrawingPointF(this Avalonia.Point pnt)
+        {
+            return new System.Drawing.PointF((float)pnt.X, (float)pnt.Y);
+        }
+
+        public static Avalonia.Point ToAvaloniaPoint(this System.Drawing.PointF pnt)
+        {
+            return new Avalonia.Point(pnt.X, pnt.Y);
+        }
+    }
+}
