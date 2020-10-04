fe3f7beb73a74f0a55b8142c4e404ca695045512
Splitwirez <Splitwirez@gmail.com>
2020-10-03 21:56:26 -0400

    `BlenderBar` is now significantly more robust

--- Mechanism.AvaloniaUI/Controls/BlenderBar/BlenderBarItem.cs
+++ Mechanism.AvaloniaUI/Controls/BlenderBar/BlenderBarItem.cs
@@ -124,6 +124,8 @@ namespace Mechanism.AvaloniaUI.Controls.BlenderBar
                 if ((selected != null) && Items.Cast<object>().Contains(selected))
                 {
                     LastSelectedItem = selected;
+                    /*if (selected is Visual vis)
+                        AttachedIcon.SetIconGap(this, AttachedIcon.GetIconGap(vis));*/
                     AnyChildSelected = true;
                     /*bool newVal = Items.Cast<object>().Contains(selected);
                     
@@ -147,42 +149,204 @@ namespace Mechanism.AvaloniaUI.Controls.BlenderBar
             }
         }
 
+        void WaitForPointer(Action action)
+        {
+            Timer timer = new Timer(100);
+            timer.Elapsed += (sneder, args) =>
+            {
+                Dispatcher.UIThread.Post(action);
+                timer.Stop();
+            };
+            timer.Start();
+        }
+
         int _millis = 0;
+        ulong _pressedTimestamp = 0;
+        DispatcherTimer _timer = new DispatcherTimer()
+        {
+            Interval = TimeSpan.FromMilliseconds(150)
+        };
+        bool _timerTickEventHandlerSet = false;
         protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            {
+                IsPressed = true;
+                /*_pressedTimestamp = e.Timestamp;
+                Console.WriteLine("_pressedTimestamp: " + _pressedTimestamp);*/
+
+                if (ItemCount > 0)
+                {
+                    //e.Handled = true;
+                    if (!_timerTickEventHandlerSet)
+                    {
+                        
+                        _timer.Tick += (snader, ergs) =>
+                        {
+                            /*_millis++;
+                            Console.WriteLine("_millis: " + _millis);*/
+                            //_timer.Stop();
+                            if (!_itemsPopup.IsOpen)
+                            {
+                                var items = VisualRoot.Renderer.HitTest(e.GetPosition(VisualRoot), this, null);
+                                /*foreach (object ob in items)
+                                    Console.WriteLine("ob type: " + ob.GetType().FullName + ", " + (ob as Visual).TemplatedParent.GetType().FullName);
+                                Console.WriteLine("items.Count(): " + items.Count());*/
+                                if (items.Contains(this) || items.OfType<Visual>().Any(x => x.TemplatedParent == this))
+                                    _itemsPopup.IsOpen = true;
+                            }
+                            /*else
+                            {
+                                
+                                //Avalonia.VisualExtensions.
+                                //var popupRoot = _itemsPopup.GetLogicalAncestors().OfType<TopLevel>().First();
+                                var popupItems = VisualRoot.Renderer.HitTest(e.GetCurrentPoint(_itemsPopup).Position, _itemsPopup, null).OfType<Visual>();
+                                var items = Items.Cast<object>().ToList();
+                                foreach (object o in items)
+                                {
+                                    BlenderBarItem bbi = null;
+                                    if (o is BlenderBarItem bb)
+                                        bbi = bb;
+                                    else
+                                    {
+                                        var container = ItemContainerGenerator.ContainerFromIndex(items.IndexOf(o));
+                                        if (container is BlenderBarItem bb2)
+                                            bbi = bb2;
+                                    }
+
+                                    bbi.SetPointerOver(popupItems.Any(x => (x == bbi) || (x.TemplatedParent == bbi)));
+                                }
+                            }*/
+                        };
+                        _timerTickEventHandlerSet = true;
+                    }
+                    _timer.Start();
+                }
+            }
+        }
+        
+        internal void SetPointerOver(bool newValue)
+        {
+            //SetValue(IsPointerOverProperty, newValue);
+        }
+
+        protected override void OnPointerReleased(PointerReleasedEventArgs e)
+        {
+            if (_timer.IsEnabled)
+                _timer.Stop();
+
+            if (IsPressed && (e.InitialPressMouseButton == MouseButton.Left))
+                IsPressed = false;
+            
+            if ((ItemCount > 0) && (_itemsPopup.IsOpen))
+            {
+                var items = Items.Cast<object>().ToList();
+                List<BlenderBarItem> bbItems = new List<BlenderBarItem>();
+                foreach (object o in items)
+                {
+                    BlenderBarItem bbi = null;
+                    if (o is BlenderBarItem bb)
+                        bbi = bb;
+                    else
+                    {
+                        var container = ItemContainerGenerator.ContainerFromIndex(items.IndexOf(o));
+                        if (container is BlenderBarItem bb2)
+                            bbi = bb2;
+                    }
+                    bbItems.Add(bbi);
+                }
+
+                
+                //Console.WriteLine("popup grandparent: " + (_itemsPopup.Parent as Panel).Parent.GetType().FullName);
+                var popupRoot = Avalonia.VisualTree.VisualExtensions.GetVisualRoot(_itemsPopup.Child);
+
+                var point = e.GetCurrentPoint(this).Position;
+                //Console.WriteLine("Original point coords: " + point.ToString() + ", " + (popupRoot == VisualRoot).ToString());
+                var pxPoint = VisualExtensions.PointToScreen(this, point); //.ToPoint(VisualRoot.RenderScaling);
+                point = VisualExtensions.PointToClient(_itemsPopup.Child, pxPoint);
+
+                var hitTestItems = popupRoot.Renderer.HitTest(point, _itemsPopup.Child, null).OfType<Visual>();
+                //hitTestItems = hitTestItems.OfType<Visual>(); //VisualRoot.Renderer.HitTest(e.GetCurrentPoint(VisualRoot).Position, _itemsPopup, null).OfType<Visual>();
+                //Console.WriteLine("hhitTestItems.Count(): " + hhitTestItems.Count() + "\nPoint coords: " + point.ToString());
+                
+                var sel = bbItems.FirstOrDefault(x => hitTestItems.Any(w => (w == x) || (w.TemplatedParent == x)));
+                if (sel != null)
+                {
+                    sel.SetPointerOver(false);
+                    GetOwnerBlenderBar().SelectedItems.Clear();
+                    GetOwnerBlenderBar().SelectedItems.Add(items.ElementAt(bbItems.IndexOf(sel)));
+                    UpdateAnyChildSelected();
+                }
+                _itemsPopup.IsOpen = false;
+            }
+            /*if ((ItemCount > 0) && (_itemsPopup.IsPointerOver))
+            {
+                var items = Items.Cast<object>().ToList();
+                foreach (object o in items)
+                {
+                    BlenderBarItem bbi = null;
+                    if (o is BlenderBarItem bb)
+                        bbi = bb;
+                    else
+                    {
+                        var container = ItemContainerGenerator.ContainerFromIndex(items.IndexOf(o));
+                        if (container is BlenderBarItem bb2)
+                            bbi = bb2;
+                    }
+                    Console.WriteLine("_itemsPopup parent type name: " + _itemsPopup.Parent.GetType().FullName);
+                    VisualRoot.Renderer.HitTest(e.GetPosition(bbi), _itemsPopup, null);
+                    //_itemsPopup.PointerLeave += (sneder, args) => UnsetSubItemEventHandlers();
+                }
+            }*/
+        }
+
+        void zOnPointerPressed(PointerPressedEventArgs e)
         {
             if (ItemCount == 0)
                 base.OnPointerPressed(e);
 
             if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            {
                 IsPressed = true;
+                _pressedTimestamp = e.Timestamp;
+            }
             
             if (ItemCount > 0)
             {
-                e.Handled = true;
+                //e.Handled = true;
                 _millis = 0;
 
                 Timer timer = new Timer(1);
                 timer.Elapsed += (snader, ergs) =>
                 {
+                    Console.WriteLine("e.Timestamp gap: " + (e.Timestamp - _pressedTimestamp).ToString());
+                    _pressedTimestamp = e.Timestamp;
                     _millis++;
                     
                     if (_millis > 300)
                     {
+                        timer.Stop();
                         Dispatcher.UIThread.Post(() =>
                         {
-                            _itemsPopup.IsOpen = true;
+                            WaitForPointer(() =>
+                            {
+                                if (IsPointerOver)
+                                    _itemsPopup.IsOpen = true;
+                            });
                         });
-                        timer.Stop();
                     }
                 };
                 timer.Start();
             }
         }
 
-        protected override void OnPointerReleased(PointerReleasedEventArgs e)
+        void zOnPointerReleased(PointerReleasedEventArgs e)
         {
             base.OnPointerReleased(e);
 
+            //Console.WriteLine("e.Timestamp gap: " + (e.Timestamp - _pressedTimestamp).ToString());
             if (IsPressed && (e.InitialPressMouseButton == MouseButton.Left))
                 IsPressed = false;
 
@@ -194,20 +358,23 @@ namespace Mechanism.AvaloniaUI.Controls.BlenderBar
                 {
                     Dispatcher.UIThread.Post(() =>
                     {
-                        var items = Items.Cast<object>().ToList();
-                        foreach (object o in items)
+                        if ((IsPointerOver || _itemsPopup.IsPointerOver) && _itemsPopup.IsOpen)
                         {
-                            if (o is BlenderBarItem bbi)
+                            var items = Items.Cast<object>().ToList();
+                            foreach (object o in items)
                             {
-                                bbi.PointerMoved += SubItem_PointerEnter;
+                                if (o is BlenderBarItem bbi)
+                                {
+                                    bbi.PointerMoved += SubItem_PointerEnter;
+                                }
+                                else
+                                {
+                                    var container = ItemContainerGenerator.ContainerFromIndex(items.IndexOf(o));
+                                    if (container is BlenderBarItem bbi2)
+                                        bbi2.PointerMoved += SubItem_PointerEnter;
+                                }
+                                //_itemsPopup.PointerLeave += (sneder, args) => UnsetSubItemEventHandlers();
                             }
-                            else
-                            {
-                                var container = ItemContainerGenerator.ContainerFromIndex(items.IndexOf(o));
-                                if (container is BlenderBarItem bbi2)
-                                    bbi2.PointerMoved += SubItem_PointerEnter;
-                            }
-                            //_itemsPopup.PointerLeave += (sneder, args) => UnsetSubItemEventHandlers();
                         }
                     });
                 }
