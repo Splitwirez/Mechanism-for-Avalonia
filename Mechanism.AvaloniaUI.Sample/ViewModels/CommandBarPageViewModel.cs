373fb7535d049d7efe592090b61314ad491ccd86
Splitwirez <Splitwirez@gmail.com>
2021-02-25 17:01:16 -0500

    Yes

--- Mechanism.AvaloniaUI.Sample/ViewModels/CommandBarPageViewModel.cs
+++ Mechanism.AvaloniaUI.Sample/ViewModels/CommandBarPageViewModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Avalonia.Controls;
+using Mechanism.AvaloniaUI.Controls;
 using Mechanism.AvaloniaUI.Sample.Views;
 
 namespace Mechanism.AvaloniaUI.Sample.ViewModels
@@ -11,6 +12,32 @@ namespace Mechanism.AvaloniaUI.Sample.ViewModels
         public CommandBarPageViewModel()
         {
             Title = "CommandBar";
+        }
+
+        bool _itemsAlignmentIsRight = false;
+        public bool ItemsAlignmentIsRight
+        {
+            get => _itemsAlignmentIsRight;
+            set 
+            {
+                _itemsAlignmentIsRight = value;
+                NotifyPropertyChanged();
+                if (_itemsAlignmentIsRight)
+                    ItemsAlignment = ChildrenHorizontalAlignment.Right;
+                else
+                    ItemsAlignment = ChildrenHorizontalAlignment.Left;
+            }
+        }
+
+        ChildrenHorizontalAlignment _itemsAlignment = ChildrenHorizontalAlignment.Left;
+        public ChildrenHorizontalAlignment ItemsAlignment
+        {
+            get => _itemsAlignment;
+            set 
+            {
+                _itemsAlignment = value;
+                NotifyPropertyChanged();
+            }
         }
     }
 }
